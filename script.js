fef72c96ba6ba9832f5fabcdbfa85dd748012077
Vlad Zobnin <vladopodobniy@gmail.com>
2020-06-25 21:14:48 +0300

    up

---
+++ script.js
@@ -0,0 +1,59 @@
+// let num = Math.floor(Math.random() * 10);
+// let guess = +prompt("Guess a number");
+
+// if(num === guess) {
+//     console.log("Bravo");
+// } else if (num < guess){
+//     console.log('Number is lower');
+// } else if (num > guess) {
+//     console.log('Number is highter');
+// }
+// else {
+//     console.log('Try again');
+// }
+
+// console.log("Game over");
+
+// let booleanString = 'true';
+// let num = 20;
+// let condition = 10;
+// let result;
+// result = booleanString === 'true' ? true : false;
+
+// let age = +prompt('Введите возраст',);
+// if (age >= 18) {
+//     console.log('Dostup');
+// } else if (age < 12) {
+//     console.log('drugoy servis');
+// } else if (18 > age && age > 12) {
+//     console.log('zapret');
+// } 
+// else {
+//     console.log('ne chislo');
+// }
+
+// let res = age >= 18 ? console.log('dostup') : 18 > age && age > 12 ? console.log('zapret') : age < 12 ? console.log('drugoy servis') : console.log('ne chislo');
+// let typeOfAction = prompt('Enter action type');
+
+// let CREATE_ACTION = 'create';
+// let READ_ACTION = 'read';
+// let UPDATE_ACTION = 'update';
+// let DELETE_ACTION = 'delete';
+
+// switch (typeOfAction) {
+//     case CREATE_ACTION:
+//         console.log('Create');
+//         break;
+//     case READ_ACTION:
+//         console.log('Read');
+//         break;
+//     case UPDATE_ACTION:
+//         console.log('Update');
+//         break;
+//     case DELETE_ACTION:
+//         console.log('Delete');
+//         break;
+//     default:
+//         console.log('Default behaviour');                
+// }   
+
